87144c74a09a7776631a7b2913ebf4847fe7a871
kinglemar <dimeji.ogunleye20@gmail.com>
2023-07-15 11:49:29 +0100

    Fix: Api bug.

--- src/views/Guidelines.vue
+++ src/views/Guidelines.vue
@@ -1,68 +1,99 @@
 <template>
   <div>
-    <h1 style="font-size: 28px; padding: 1rem;">Trending news</h1>
+    <h1 style="font-size: 28px; padding: 1rem">Trending news</h1>
     <div class="size">
-      <div class="gallery">
+      <div>
+        <div class="gallery" v-if="!preloader">
+          <NewsCard
+            v-for="(info, index) in news"
+            :key="index"
+            :jpegUrl="info?.media"
+            :title="info?.title"
+            :date="info?.published_date"
+            :content="info?.summary"
+          />
+        </div>
 
-          <NewsCard v-if="!preloader" v-for="(info, index) in news" :key="index" :jpegUrl="info.urlToImage" :title="info.title"
-            :date="info.pubDate" :content="info.content" />
 
-
-          <Skeleton v-if="preloader" v-for="(loader, index) in loaders" :key="index" />
+          <div class="gallery" v-if="preloader">
+            <Skeleton v-for="(loader, index) in loaders" :key="index" />
+          </div>
       </div>
     </div>
 
-    <b-pagination @change="nextPage" pills align="center" :total-rows="rows" v-model="page" :per-page="perPage"
-      :limit="5"></b-pagination>
+    <div class="mt-5" v-if="true">
+      <b-pagination
+        @change="nextPage"
+        pills
+        align="center"
+        :total-rows="rows"
+        v-model="page"
+        :per-page="perPage"
+        :limit="5"
+      ></b-pagination>
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-import NewsCard from '../components/NewsCard.vue'
-import Skeleton from '../components/Skeleton.vue'
+import axios from "axios";
+import NewsCard from "../components/NewsCard.vue";
+import Skeleton from "../components/Skeleton.vue";
 export default {
-  name: 'Guidelines',
+  name: "Guidelines",
   components: { NewsCard, Skeleton },
   data() {
     return {
       news: [],
       preloader: true,
-      loaders: [1, 2 ,3, 5],
+      loaders: [1, 2, 3, 5],
       page: 1,
       perPage: 10,
       rows: 220,
-      currentPage: 1
-    }
+      currentPage: 1,
+    };
   },
   mounted() {
-    this.getLatestNews()
+    this.getLatestNews();
   },
   methods: {
     async getLatestNews() {
-      this.preloader = true
-      let url = `https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/news/get-coronavirus-news/${this.currentPage}`
+      this.preloader = true;
+      let url = `https://coronavirus-smartable.p.rapidapi.com/news/v1/US/${this.currentPage}`;
 
       const options = {
-        method: 'GET',
+        method: "GET",
+        url: "https://covid-19-news.p.rapidapi.com/v1/covid",
+        params: {
+          q: "covid",
+          lang: "en",
+          media: "True",
+          page: this.currentPage,
+          page_size: 20
+        },
         headers: {
-          'X-RapidAPI-Key': '45e2dfa1camsh523c84837050448p17a1d2jsn6a6b6c138e88',
-          'X-RapidAPI-Host': 'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com'
-        }
+          "X-RapidAPI-Key":
+            "45e2dfa1camsh523c84837050448p17a1d2jsn6a6b6c138e88",
+          "X-RapidAPI-Host": "covid-19-news.p.rapidapi.com",
+        },
       };
-      let response = await axios(url, options)
-      console.log(response.data.news)
-      this.news = response.data.news
-
-      this.preloader = false
+      try {
+        const response = await axios.request(options);
+        console.log(response.data?.articles);
+        this.news = response?.data?.articles || []
+        this.preloader = false;
+      } catch (error) {
+        console.error(error);
+        this.preloader = false
+      }
     },
 
     nextPage(e) {
-      this.currentPage = e
-      this.getLatestNews()
-    }
-  }
-}
+      this.currentPage = e;
+      this.getLatestNews();
+    },
+  },
+};
 </script>
 
 <style scoped>
